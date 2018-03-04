523393855f6167262b6cf99681591b810edae8be
rhoboro <rhoboro@gmail.com>
2018-03-04 10:05:06 +0900

    予約APIを叩く

--- src/yadokari.py
+++ src/yadokari.py
@@ -1,14 +1,30 @@
+import json
+
 import click
+import requests
+
+BASE_URL = 'https://yado-kari.herokuapp.com/'
 
 RESERVATION_INFO = '''
 *** YOUR INFORMATION ***
-0. check in date: {check_in_date}
-1. check out date: {check_out_date}
-2. your name: {name}
-3. email address: {email}
-4. how many people: {num_of_pople}
+{}
 ************************
 '''
+
+VALUE_TO_PROMPT = [
+    ('name', 'your name', None, None),
+    ('email', 'email address', None, None),
+    ('tel', 'tel number', None, None),
+    ('check_in_on', 'check in date', None, None),
+    ('check_out_on', 'check out date', None, None),
+    ('check_in_time', 'check in time', None, None),
+    ('men_number', 'number of men', None, None),
+    ('women_number', 'number of women', None, None),
+    ('purpose_of_use', 'purpose of use', '開発合宿', True),
+    ('payment_method', 'method of payment', '現金支払い', True),
+    ('coupon', 'coupon code', '無し', True),
+    ('note', 'note', '無し', True),
+]
 
 
 @click.group()
@@ -19,7 +35,9 @@ def cmd():
 @cmd.command(name='list')
 def show_list():
     """宿の一覧を表示 """
-    click.echo('omishima-space')
+    res = requests.get(BASE_URL + '/api/v1/yados')
+    yados = json.loads(res.content)
+    click.echo('\n'.join(f'{yado["id"]}. {yado["name"]}' for yado in yados))
 
 
 @cmd.command()
@@ -40,35 +58,46 @@ def cal(context, year, month):
     print(calendar.month(year, month))
 
 
-VALUE_TO_PROMPT = [
-    ('check_in_date', lambda: click.prompt('check in date')),
-    ('check_out_date', lambda: click.prompt('check out date')),
-    ('name', lambda: click.prompt('your name')),
-    ('email', lambda: click.prompt('email address')),
-    ('num_of_pople', lambda: click.prompt('how many people')),
-]
-
-
 @cmd.command()
 @click.argument('context', nargs=-1)
 def reserve(context):
     """宿の予約を行う """
-    info = {}
-    for i, m in VALUE_TO_PROMPT:
-        info[i] = m()
+    info = get_reservation_info()
+    res = requests.post(BASE_URL + 'api/v1/yados/1/reservations',
+                        data={f'reservation[{k}]': v for k, v in info.items()})
+    click.echo(res.content)
+    click.echo('done!!')
 
-    click.echo(RESERVATION_INFO.format(**info))
-    confirm = click.prompt('confirm(Y/y) or edit(0-{}): '.format(len(VALUE_TO_PROMPT)))
+
+def get_reservation_info():
+    """ユーザーから宿の予約情報を受け取る """
+    info = {}
+    for i, text, default, show_default in VALUE_TO_PROMPT:
+        info[i] = click.prompt(text, default=default, show_default=show_default)
+
+    click.echo(RESERVATION_INFO.format(
+        '\n'.join((f'{i} {t[1]}: {info[t[0]]}' for i, t in enumerate(VALUE_TO_PROMPT)))))
+    confirm = click.prompt('confirm(Y/y) or edit(0-{})'.format(len(VALUE_TO_PROMPT) - 1))
     while confirm not in ('Y', 'y', 'YES', 'Yes', 'yes'):
         try:
             confirm = int(confirm)
-            i, m = VALUE_TO_PROMPT[confirm]
-            info[i] = m()
+            i, text, default, show_default = VALUE_TO_PROMPT[confirm]
+            info[i] = click.prompt(text, default=default, show_default=show_default)
         except:
             pass
-        click.echo(RESERVATION_INFO.format(**info))
-        confirm = click.prompt('confirm(Y/y) or edit(0-{}): '.format(len(VALUE_TO_PROMPT)))
-    click.echo('done!!')
+        click.echo(RESERVATION_INFO.format(
+            '\n'.join((f'{i} {t[1]}: {info[t[0]]}' for i, t in enumerate(VALUE_TO_PROMPT)))))
+        confirm = click.prompt('confirm(Y/y) or edit(0-{}): '.format(len(VALUE_TO_PROMPT) - 1))
+    return info
+
+
+@cmd.command()
+@click.argument('token', nargs=-1)
+def me(token):
+    if not token:
+        return
+    res = requests.get(BASE_URL + f'api/v1/me/{token}')
+    click.echo(res.text)
 
 
 def main():
