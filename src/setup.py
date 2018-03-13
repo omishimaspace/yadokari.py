7ecc1f85f4f5f68847330d77386b1a3c029fe7fb
rhoboro <rhoboro@gmail.com>
2018-03-13 19:44:35 +0900

    諸々改善

--- src/setup.py
+++ src/setup.py
@@ -2,16 +2,17 @@ from setuptools import setup
 
 setup(
     name='yadokari',
-    version='0.1.2',
+    version='0.1.3',
     author='rhoboro',
     author_email='rhoboro@gmail.com',
     maintainer='rhoboro',
     maintainer_email='rhoboro@gmail.com',
     license="MIT",
     py_modules=['yadokari'],
+    description='yadokari.py is a command line tool for yadokari reservation system.',
     install_requires=[
-        'Click',
-        'requests',
+        'Click<7.0',
+        'requests<3.0',
     ],
     entry_points='''
         [console_scripts]
--- src/yadokari.py
+++ src/yadokari.py
@@ -5,6 +5,7 @@ import click
 import requests
 
 BASE_URL = 'https://yado-kari.herokuapp.com/'
+API_URL = BASE_URL + 'api/v1'
 
 RESERVATION_INFO = '''
 *** YOUR INFORMATION ***
@@ -36,30 +37,41 @@ def cmd():
 @cmd.command(name='list')
 def show_list():
     """宿の一覧を表示 """
-    res = requests.get(BASE_URL + '/api/v1/yados')
+    res = requests.get(API_URL + '/yados')
+    if res.status_code == 404:
+        click.echo('not found')
+        return
     yados = json.loads(res.content)
     click.echo('\n'.join(('{}. {}'.format(yado["id"], yado["name"]) for yado in yados)))
 
 
 @cmd.command()
-@click.argument('context', nargs=-1)
+@click.argument('context', nargs=1)
 def show(context):
     """宿の詳細URLを表示 """
-    if context:
-        click.echo('https://omishima-space.com/')
+    res = requests.get(API_URL + '/yados/{}'.format(context))
+    if res.status_code == 404:
+        click.echo('not found')
+        return
+    yado = json.loads(res.content)
+    click.echo('{}'.format(yado['url']))
 
 
 @cmd.command()
 @click.argument('context', nargs=1)
 def cal(context):
     """宿の予約状況を表示 """
-    if not context:
-        context = 1
-    res = requests.get(BASE_URL + '/api/v1/yados/{}/schedules'.format(context))
+    res = requests.get(API_URL + '/yados/{}/schedules'.format(context))
+    if res.status_code == 404:
+        click.echo('not found')
+        return
     reservations = json.loads(res.content)
-    click.echo(
-        '\n'.join('{}: {} - {}'.format(r["schedule"], r["started_on"], r["finished_on"]) for r in
-                  reservations))
+    if reservations:
+        click.echo('\n'.join(
+            '{}: {} - {}'.format(r["schedule"], r["started_on"], r["finished_on"])
+            for r in reservations))
+    else:
+        click.echo('no reservation found')
 
 
 @cmd.command()
@@ -68,23 +80,23 @@ def reserve(context):
     """宿の予約を行う """
     if not context:
         context = 1
-    info = get_reservation_info()
-    res = requests.post(BASE_URL + 'api/v1/yados/{}/reservations'.format(context),
+    info = _get_reservation_info()
+    res = requests.post(API_URL + 'api/v1/yados/{}/reservations'.format(context),
                         data={'reservation[{}]'.format(k): v for k, v in info.items()})
-    click.echo(res.content)
-    click.echo('done!!')
+    if res.status_code == 200:
+        click.echo(res.content)
+        click.echo('done!!')
+    else:
+        click.echo('some errors occurred')
 
 
-def get_reservation_info():
+def _get_reservation_info():
     """ユーザーから宿の予約情報を受け取る """
     info = {}
     for i, text, default, show_default in VALUE_TO_PROMPT:
         info[i] = click.prompt(text, default=default, show_default=show_default)
 
-    click.echo(RESERVATION_INFO.format(
-        '\n'.join(
-            ('{}. {}: {}'.format(i, t[1], info[t[0]]) for i, t in enumerate(VALUE_TO_PROMPT)))))
-    confirm = click.prompt('confirm(Y/y) or edit(0-{})'.format(len(VALUE_TO_PROMPT) - 1))
+    confirm = _ask_confirm(info)
     while confirm not in ('Y', 'y', 'YES', 'Yes', 'yes'):
         try:
             confirm = int(confirm)
@@ -92,20 +104,23 @@ def get_reservation_info():
             info[i] = click.prompt(text, default=default, show_default=show_default)
         except:
             pass
-        click.echo(RESERVATION_INFO.format(
-            '\n'.join(
-                ('{}. {}: {}'.format(i, t[1], info[t[0]]) for i, t in enumerate(VALUE_TO_PROMPT)))))
-        confirm = click.prompt('confirm(Y/y) or edit(0-{}): '.format(len(VALUE_TO_PROMPT) - 1))
+        confirm = _ask_confirm(info)
     return info
+
+
+def _ask_confirm(info):
+    """ユーザーに宿の予約情報の確認を行う """
+    click.echo(RESERVATION_INFO.format(
+        '\n'.join(('{}. {}: {}'.format(i, t[1], info[t[0]])
+                   for i, t in enumerate(VALUE_TO_PROMPT)))))
+    return click.prompt('confirm(Y/y) or edit(0-{})'.format(len(VALUE_TO_PROMPT) - 1))
 
 
 @cmd.command()
 @click.argument('token', nargs=1)
 def me(token):
     """トークンから予約情報を受け取る """
-    if not token:
-        return
-    res = requests.get(BASE_URL + 'api/v1/me/{}'.format(token))
+    res = requests.get(API_URL + 'api/v1/me/{}'.format(token))
     if res.status_code == 404:
         click.echo('not found')
         return
